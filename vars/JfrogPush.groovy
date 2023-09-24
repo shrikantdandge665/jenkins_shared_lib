e6784ccb7505319f526d0a8a2ae90565f2cf4cdb
shrikantdandge665 <55130251+shrikantdandge665@users.noreply.github.com>
2023-09-24 16:19:56 +0530

    Update JfrogPush.groovy

--- vars/JfrogPush.groovy
+++ vars/JfrogPush.groovy
@@ -1,5 +1,6 @@
 def call(credentialsId){
   ARTIFACTORY_ACCESS_TOKEN = credentials('artifactory-access-token')
   JFROG_PASSWORD = credentials('jfrog-password')
+  jf rt upload --url http://100.25.200.113:8082/artifactory/ --access-token ${ARTIFACTORY_ACCESS_TOKEN} target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar java-web-app/
 }
   
