886136d68dd01ea3c143a9b4766cd110bcca1bed
shrikantdandge665 <55130251+shrikantdandge665@users.noreply.github.com>
2023-09-29 16:08:19 +0530

    Update JfrogPush.groovy

--- vars/JfrogPush.groovy
+++ vars/JfrogPush.groovy
@@ -1,11 +1,10 @@
 def call(String version, String artifactPath, String targetPath) {
-        withCredentials([usernamePassword(credentialsId: 'jfrog', usernameVariable: 'USER', passwordVariable: 'PASS')])    
-        try {
-            //sh "jfrog rt u ${artifactPath} ${targetPath} --user=${username} --password=${password}"
+        withCredentials([usernamePassword(
+            credentialsId: 'jfrog',
+            usernameVariable: 'USER',
+            passwordVariable: 'PASS'
+        )]) {
             sh "curl -u $USER:$PASS -X PUT -T ${artifactPath} ${targetPath}"
             echo "Uploaded JAR artifact to Artifactory: ${targetPath}"
-        } 
-        catch (Exception e) {
-            error "Failed to upload JAR artifact to Artifactory: ${e.message}"
-        }
+        }      
 }
